89a40614ceef14bc98dba7484b241a8176bddbba
Andrew Maguire <andrewm4894@gmail.com>
2017-01-24 23:36:39 +0000

    Create get_os.R

---
+++ get_os.R
@@ -0,0 +1,16 @@
+##########################################
+## FUNCTION TO GET OS
+##########################################
+
+# taken from https://github.com/hadley/rappdirs/blob/master/R/utils.r#L1
+get_os <- function() {
+  if (.Platform$OS.type == "windows") { 
+    "win"
+  } else if (Sys.info()["sysname"] == "Darwin") {
+    "mac" 
+  } else if (.Platform$OS.type == "unix") { 
+    "unix"
+  } else {
+    stop("Unknown OS")
+  }
+}
